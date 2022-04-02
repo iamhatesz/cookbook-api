11751b0213672860d14a1cbe757a4d04d5612f20
Tomasz Wrona <tomasz@wrona.me>
2022-04-02 22:47:55 +0200

    Added dj-database-url

--- heroku_rest_api/settings.py
+++ heroku_rest_api/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import os
 from pathlib import Path
 
+import dj_database_url
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -83,10 +84,7 @@ WSGI_APPLICATION = "heroku_rest_api.wsgi.application"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(conn_max_age=600, ssl_require=True),
 }
 
 
