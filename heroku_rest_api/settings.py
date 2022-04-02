d039831db19200ee071820598a4a9bea11f388e7
Tomasz Wrona <tomasz@wrona.me>
2022-04-02 22:13:54 +0200

    Use env

--- heroku_rest_api/settings.py
+++ heroku_rest_api/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +20,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-nktx%3uvo_!-cg*om3r_p8!vxfn5c$6!7j(soldpnh(l56va%k"
+assert "SECRET_KEY" in os.environ, "`SECRET_KEY` must be set."
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+assert "ENVIRONMENT" in os.environ, "`ENVIRONMENT` must be set."
+DEBUG = os.environ["ENVIRONMENT"] != "production"
 
-ALLOWED_HOSTS = []
+assert "HOSTNAME" in os.environ, "`HOSTNAME` must be set."
+ALLOWED_HOSTS = [os.environ["HOSTNAME"]]
 
 
 # Application definition
