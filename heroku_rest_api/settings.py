20c2eda4db0bea4b1bc6f81934860fb32fdd6054
Tomasz Wrona <tomasz@wrona.me>
2022-04-02 22:25:25 +0200

    Added whitenoise

--- heroku_rest_api/settings.py
+++ heroku_rest_api/settings.py
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = "heroku_rest_api.urls"
@@ -122,6 +123,8 @@ USE_TZ = True
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATIC_URL = "/static/"
 STATICFILES_DIRS = (BASE_DIR / "static",)
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
